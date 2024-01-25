1e1e907400f2b07888a658abb9dfbc77745756c4
jiyeon-dev <happysophiapark@gmail.com>
2024-01-25 16:11:56 +0900

    feat: create userInput component

--- React-TheCompleteGuide2024/02-investment-calculator/src/App.jsx
+++ React-TheCompleteGuide2024/02-investment-calculator/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => ({
+      ...prevUserInput,
+      [inputIdentifier]: newValue,
+    }));
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+    </>
+  );
 }
 
 export default App;
