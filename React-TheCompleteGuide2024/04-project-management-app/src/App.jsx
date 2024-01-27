35ed957013dc13b61471ef98223dbd141ab1fac4
jiyeon-dev <happysophiapark@gmail.com>
2024-01-28 00:52:44 +0900

    feat: create project logic and show project list on sidebar

--- React-TheCompleteGuide2024/04-project-management-app/src/App.jsx
+++ React-TheCompleteGuide2024/04-project-management-app/src/App.jsx
@@ -1,13 +1,59 @@
+import { useState } from "react";
+import { v4 as uuid4 } from "uuid";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined, // undefined: no selected | null: create new project | project id: selected project
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProject: null,
+    }));
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProject: undefined,
+    }));
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: uuid4(),
+      };
+
+      return {
+        ...prevState,
+        selectedProject: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProject === null)
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
+  else if (projectsState.selectedProject === undefined)
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
+      {content}
     </main>
   );
 }
--- React-TheCompleteGuide2024/04-project-management-app/src/components/ProjectsSidebar.jsx
+++ React-TheCompleteGuide2024/04-project-management-app/src/components/ProjectsSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./ui/Button";
 
-export default function ProjectsSidebar() {
+export default function ProjectsSidebar({ onStartAddProject, projects }) {
   return (
     <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl'>
       <h2 className='mb-8 font-bold uppercase md:text-xl text-stone-200'>
@@ -8,13 +8,20 @@ export default function ProjectsSidebar() {
       </h2>
 
       <div>
-        <Button>+ Add Project</Button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
 
       <ul className='mt-8 bg-stone'>
-        {/* <button className='w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800'>
-          Learning React
-        </button> */}
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button
+              key={project.id}
+              className='w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800'
+            >
+              {project.title}
+            </button>
+          </li>
+        ))}
       </ul>
     </aside>
   );
