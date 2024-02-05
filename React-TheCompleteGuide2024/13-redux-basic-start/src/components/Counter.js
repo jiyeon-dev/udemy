eb49bb232988bb1b27c190492cc68ed0a22d29db
jiyeon-dev <happysophiapark@gmail.com>
2024-02-06 01:44:16 +0900

    feat: learning  mange multiple state values in redux

--- React-TheCompleteGuide2024/13-redux-basic-start/src/components/Counter.js
+++ React-TheCompleteGuide2024/13-redux-basic-start/src/components/Counter.js
@@ -4,22 +4,29 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
+  };
+  const increaseHandler = (num) => {
+    dispatch({ type: "increase", amount: num });
   };
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
+        <button onClick={() => increaseHandler(5)}>increment by 5</button>
         <button onClick={decrementHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
