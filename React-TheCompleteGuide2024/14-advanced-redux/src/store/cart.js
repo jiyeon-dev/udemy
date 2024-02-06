1af5e7bbdb146b17bf05070eb73e7a7d4cb98ead
jiyeon-dev <happysophiapark@gmail.com>
2024-02-06 19:53:30 +0900

    feat: create add/remove item in cart function using redux

---
+++ React-TheCompleteGuide2024/14-advanced-redux/src/store/cart.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: { items: [], totalQuantity: 0 },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.itemId === newItem.id
+      );
+
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          name: newItem.title,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      } else {
+        // redux-toolkit이라 기존 항목에 접근하여 업데이트 가능한 것임. 일반 redux였으면 안됨.
+        existingItem.quantity++;
+        existingItem.totalPrice += existingItem.price;
+      }
+
+      state.totalQuantity++;
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+
+      state.totalQuantity--;
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
