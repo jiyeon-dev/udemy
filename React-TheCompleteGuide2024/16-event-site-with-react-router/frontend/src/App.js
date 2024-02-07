b0811730c9ee03f3c9c50bb50f24b86682da8513
jiyeon-dev <happysophiapark@gmail.com>
2024-02-07 21:12:43 +0900

    feat: 동적 라우트와 loader, 부모 route의 loader사용하는 방법

--- React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/App.js
+++ React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/App.js
@@ -24,7 +24,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -46,8 +48,15 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ":eventId", element: <EventDetailPage /> },
-          { path: ":eventId/edit", element: <EditEventPage /> },
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              { index: true, element: <EventDetailPage /> },
+              { path: "edit", element: <EditEventPage /> },
+            ],
+          },
           { path: "new", element: <NewEventPage /> },
         ],
       },
--- React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/pages/EventDetail.jsx
+++ React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/pages/EventDetail.jsx
@@ -1,11 +1,24 @@
-import { useParams } from "react-router-dom";
+import { json, useLoaderData, useRouteLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  const params = useParams();
-  return (
-    <>
-      <h1>EventDetail</h1>
-      <p>{params.eventId}</p>
-    </>
-  );
+  const data = useRouteLoaderData("event-detail");
+  const event = data.event;
+  return <EventItem event={event} />;
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  // return fetch(`http://localhost:8080/events/${id}`);
+  const response = await fetch(`http://localhost:8080/events/${id}`);
+
+  if (!response.ok) {
+    json(
+      { message: "Could not fetch details for selected event." },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
 }
