e5ab0e088218e28b0f1836deab5da522d8219668
jiyeon-dev <happysophiapark@gmail.com>
2024-02-07 20:49:10 +0900

    feat: loader & errorElement

--- React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/App.js
+++ React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/App.js
@@ -23,23 +23,29 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
           { path: "new", element: <NewEventPage /> },
--- React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/pages/Events.jsx
+++ React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/pages/Events.jsx
@@ -1,21 +1,35 @@
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Some event" },
-  { id: "e2", title: "Another event" },
-];
+import { json, useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 export default function EventsPage() {
+  const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
+
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
+}
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events" };
+    // throw { message: "Could not fetch events" };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    // throw new Error("Could not fetch");
+    throw json({ message: "Could not fetch events." }, { status: 500 });
+  } else {
+    return response;
+    // const resData = await response.json();
+    // const res = new Response(resData.events, { status: 201 });
+    // return res;
+  }
 }
