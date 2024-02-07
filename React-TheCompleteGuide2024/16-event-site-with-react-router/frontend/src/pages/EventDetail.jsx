840bfe35138041372957b08ca50d73f1dae2ed6b
jiyeon-dev <happysophiapark@gmail.com>
2024-02-08 00:03:28 +0900

    feat: 여러 개 loader사용하는 경우 defer로 관리 방법

--- React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/pages/EventDetail.jsx
+++ React-TheCompleteGuide2024/16-event-site-with-react-router/frontend/src/pages/EventDetail.jsx
@@ -1,21 +1,33 @@
 import {
+  Await,
+  defer,
   json,
   redirect,
   useLoaderData,
   useRouteLoaderData,
 } from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 export default function EventDetailPage() {
-  const data = useRouteLoaderData("event-detail");
-  const event = data.event;
-  return <EventItem event={event} />;
+  const { event, events } = useRouteLoaderData("event-detail");
+
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>{(event) => <EventItem event={event} />}</Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(events) => <EventsList events={events} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 }
 
-export async function loader({ request, params }) {
-  const id = params.eventId;
-
-  // return fetch(`http://localhost:8080/events/${id}`);
+const loadEvent = async (id) => {
   const response = await fetch(`http://localhost:8080/events/${id}`);
 
   if (!response.ok) {
@@ -24,8 +36,40 @@ export async function loader({ request, params }) {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+};
+
+const loadEvents = async () => {
+  const response = await fetch("http://localhost:8080/events");
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events" };
+    // throw { message: "Could not fetch events" };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    // throw new Error("Could not fetch");
+    throw json({ message: "Could not fetch events." }, { status: 500 });
+  } else {
+    // return response;
+
+    const resData = await response.json();
+    return resData.events;
+
+    // const resData = await response.json();
+    // const res = new Response(resData.events, { status: 201 });
+    // return res;
+  }
+};
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  return defer({
+    event: await loadEvent(id), // 이 데이터 로딩된 후에 페이지 이동
+    events: loadEvents(), // 페이지 이동 후 렌더링 된 후에 데이터 조회
+  });
 }
 
 export async function action({ request, params }) {
