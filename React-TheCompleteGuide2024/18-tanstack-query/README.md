4b3c68e72b19707eb9c1fe183f092975263e56d9
jiyeon-dev <happysophiapark@gmail.com>
2024-02-08 21:58:55 +0900

    refactor: 동적 쿼리 함수 - events 검색어

--- React-TheCompleteGuide2024/18-tanstack-query/README.md
+++ React-TheCompleteGuide2024/18-tanstack-query/README.md
@@ -54,6 +54,11 @@ export default function App() {
   - 그래서 직접 요청 전송 코드를 직접 작성해야함.
   - axios(서드 파티 라이브러리), fetch 와 같은 걸로 요청 전송 가능.
   - 그런데 useQuery는 `Promise를 반환하는 함수가 필요한 것`이기 때문에 꼭 요청을 전송할 필요는 없음.
+  - QueryFunctionContext 매개변수를 받음
+    - meta: query에 대한 추가 정보 메모 (string)
+    - queryKey
+    - signal: AbortSignal 을 의미 (네트워크 요청을 중간에 중단시킬 수 있는 장치). GET요청 시 signal옵션을 넣어주면 컴포넌트 unmount시 자동으로 네트워크 취소됨
+      > 모든 GET요청에 Abort Signal을 넣으면 작업부하를 올릴뿐 바람직하지 않음. 동영상 다운로드같은 대용량 fetching이 아닌이상 대부분의 GET요청을 빠르게 완료 및 캐싱되기 때문에 성능에 유의미한 영향을 끼치지 못하기 때문.
 - **queryKey**: 쿼리 키(배열)
   - useQuery사용할 때 모든 쿼리를 전송하는 모든 GET HTTP요청에는 쿼리 키가 있음.
   - tanstack 쿼리는 내부에서 이 쿼리 키를 이용해 요청으로 `생성된 데이터를 캐시 처리`함. 그래서 나중에 동일한 요청을 전송하면 이전 요청의 응답을 재사용 가능.
@@ -65,11 +70,17 @@ export default function App() {
 - **gcTime**: 가비지 컬렉션 시간 설정(단위 : ms)
   - 기본 값 : 5분
   - 설정 시간 만큼만 캐시에 데이터를 저장한 후 폐기함.
+- **enabled**(boolean): false인 경우 쿼리가 비활성화되어 요청이 전송되지 않음.
+  - 기본 값 : true
+  - 쿼리가 비활성화되면 tanstack쿼리가 상태를 대기중으로 처리했기 때문에 isPending값이 true가 됨 (데이터가 없으니 데이터가 오기를 (활성화되기를) 기다리고 있기 때문에 대기중인 상태로 표시되는 것임.)
 
 [응답 데이터 - 객체]
 
 - data: 실제 응답 데이터 값
-- isPending(boolean): 요청 중인지 체크(loading)
+- isPending(boolean): 데이터 요청 중인지 체크
+  - 쿼리가 비활성화(enabled = false)되면 true로 표시됨.
+- isLoading(boolean): 데이터 요청 중인지 체크
+  - isPending과는 다르게 쿼리가 비활성화되었다고 해서 true로 표시되지 않음.
 - isError(boolean): 오류 응답 받았는지 체크
 - error: 발생한 오류에 대한 정보 (message, name, stack)
 - refetch(function): 동일한 쿼리 수동으로 호출
--- React-TheCompleteGuide2024/18-tanstack-query/src/components/Events/FindEventSection.jsx
+++ React-TheCompleteGuide2024/18-tanstack-query/src/components/Events/FindEventSection.jsx
@@ -1,26 +1,66 @@
-import { useRef } from 'react';
+import { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState(undefined);
+
+  const { data, isLoading, isError, error } = useQuery({
+    // ref.current.value 하면 검색을 해도 컴포넌트가 리렌더링 되지 않기 때문에 값이 동적으로 안바뀐다.
+    // 그래서 state를 추가로 선언하여 검색을 누르면 searchTerm값을 변경하여 리렌더링한 후 queryKey, queryFn의 전달 값도 변경되도록 state 값으로 설정함.
+    queryKey: ["events", { search: searchTerm }],
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    enabled: searchTerm !== undefined,
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+  if (isLoading) {
+    content = <LoadingIndicator />;
+  }
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title='An error occurred'
+        message={error.info?.message || "Failed to fetch events."}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className='events-list'>
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
-    <section className="content-section" id="all-events-section">
+    <section className='content-section' id='all-events-section'>
       <header>
         <h2>Find your next event!</h2>
-        <form onSubmit={handleSubmit} id="search-form">
+        <form onSubmit={handleSubmit} id='search-form'>
           <input
-            type="search"
-            placeholder="Search events"
+            type='search'
+            placeholder='Search events'
             ref={searchElement}
           />
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
